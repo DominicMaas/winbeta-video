5489e3ce857d5c04c2d9516a5f113817d23eb209
Dominic Maas <dominic.maas@live.com>
2015-10-08 19:11:34 +1300

    Created Basic UI and added in basic YouTube support.

---
+++ WinBeta Videos/WinBeta Videos/Helper/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinBeta_Videos.Helper
+{
+    class Services
+    {
+        public static string GetApiKey() { return "AIzaSyBAle-H159PczwWAFOjZdH-SD2s0NqisGQ"; }
+    }
+}
--- WinBeta Videos/WinBeta Videos/MainPage.xaml.cs
+++ WinBeta Videos/WinBeta Videos/MainPage.xaml.cs
@@ -1,19 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace WinBeta_Videos
 {
@@ -22,9 +18,102 @@ namespace WinBeta_Videos
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        // The apps accent colour (WinBetas theme)
+        Color AppAccent = new Color()
+        {
+            R = 0,
+            G = 120,
+            B = 215
+        };
+
+        Color AppAccentHover = new Color()
+        {
+            R = 0,
+            G = 131,
+            B = 235
+        };
+
+        YouTubeService youtubeService;
+        string WinBetaChannelId = "UC70UzaroFf5GcyecHOGw-tw";
+        ObservableCollection<Video> videos_data;
+
         public MainPage()
         {
             this.InitializeComponent();
+            Loaded += OnPageLoaded;
+
+            // Get the Application View
+            var applicationView = ApplicationView.GetForCurrentView();
+
+            // Set the Titlebar Colours
+            applicationView.TitleBar.BackgroundColor = AppAccent;
+            applicationView.TitleBar.ButtonBackgroundColor = AppAccent;
+
+            applicationView.TitleBar.ButtonHoverBackgroundColor = AppAccentHover;
+            applicationView.TitleBar.ButtonHoverForegroundColor = Colors.White;
+
+            applicationView.TitleBar.ForegroundColor = Colors.White;
+            applicationView.TitleBar.ButtonForegroundColor = Colors.White;
+        }
+
+        private async void OnPageLoaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize the YouTube Service
+            youtubeService = new YouTubeService(new BaseClientService.Initializer() {
+                ApiKey = Helper.Services.GetApiKey(),
+                ApplicationName = "WinBeta Videos"
+            });
+
+            try
+            {
+                await GetVideos();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private async Task GetVideos()
+        {
+            videos_data = null;
+            videos_data = new ObservableCollection<Video>();
+            DataContext = videos_data;
+
+            var searchChannelRequest = youtubeService.Channels.List("contentDetails");
+            searchChannelRequest.Id = WinBetaChannelId;
+            searchChannelRequest.MaxResults = 1;
+
+            var searchChannelResponse = await searchChannelRequest.ExecuteAsync();
+            var channel = searchChannelResponse.Items.First();
+
+            var channelUploadID = channel.ContentDetails.RelatedPlaylists.Uploads;
+
+            var playlistRequest = youtubeService.PlaylistItems.List("snippet");
+            playlistRequest.PlaylistId = channelUploadID;
+            playlistRequest.MaxResults = 50;
+
+            var playlistResponse = await playlistRequest.ExecuteAsync();
+
+            foreach (var playlistItem in playlistResponse.Items)
+            {
+                Video video = new Video() {
+                    Title = playlistItem.Snippet.Title,
+                    Thumbnail = playlistItem.Snippet.Thumbnails.High.Url
+                };
+
+                videos_data.Add(video);
+            }
+        }
+
+        public class Video
+        {
+            public String Title { get; set; }
+            public String VideoId { get; set; }
+            public String Thumbnail { get; set; }
+            public String Date { get; set; }
+            public String ViewCount { get; set; }
         }
     }
 }
